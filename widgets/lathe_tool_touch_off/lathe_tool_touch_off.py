a504f1f80412e4140ba77aa8cdc2afee5a4ac3a0
TurBoss <j.l.toledano.l@gmail.com>
2020-06-21 07:27:03 +0200

    dictionary settings for lathe images

--- widgets/lathe_tool_touch_off/lathe_tool_touch_off.py
+++ widgets/lathe_tool_touch_off/lathe_tool_touch_off.py
@@ -16,6 +16,7 @@ from qtpy.QtCore import Signal, Slot, QUrl, QObject
 from qtpy.QtQuickWidgets import QQuickWidget
 
 from qtpyvcp.plugins import getPlugin
+from qtpyvcp.utilities.settings import getSetting, setSetting
 
 STATUS = getPlugin('status')
 WIDGET_PATH = os.path.dirname(os.path.abspath(__file__))
@@ -23,7 +24,8 @@ WIDGET_PATH = os.path.dirname(os.path.abspath(__file__))
 
 class LatheToolTouchOff(QQuickWidget):
 
-    toolOrientationSig = Signal(int, arguments=['active_tool_orientation'])
+    toolActiveImageSig = Signal(str, int, arguments=['group', 'index'])
+    toolResetSig = Signal()
 
     def __init__(self, parent=None):
         super(LatheToolTouchOff, self).__init__(parent)
@@ -36,9 +38,29 @@ class LatheToolTouchOff(QQuickWidget):
         url = QUrl.fromLocalFile(os.path.join(WIDGET_PATH, "lathe_tool_touch_off.qml"))
         self.setSource(url)
 
+        self.tool_image_setting = getSetting('tool-touch-off.tool-image-table')
+        self.tool_image = self.tool_image_setting.getValue()
+
         self.stat.tool_in_spindle.onValueChanged(self.set_active_tool)
 
     def set_active_tool(self):
-        num = self.stat.tool_in_spindle.getValue()
-        orientation = self.stat.tool_table[num].orientation
-        self.toolOrientationSig.emit(orientation)
+        tool_num = self.stat.tool_in_spindle.getValue()
+        
+        if tool_num not in self.tool_image.keys():
+            self.toolResetSig.emit()
+        else:
+            group, index = self.tool_image[tool_num]
+            self.toolActiveImageSig.emit(group, index)
+
+    @Slot()
+    def reset_tools(self):
+        tool_num = self.stat.tool_in_spindle.getValue()
+        self.tool_image.pop(tool_num, None)
+        self.toolResetSig.emit()
+
+    @Slot(str, int)
+    def tool_select(self, group, index):
+        tool_num = self.stat.tool_in_spindle.getValue()
+        self.tool_image[tool_num] = [group, index]
+
+        setSetting('tool-touch-off.tool-image-table', self.tool_image)
