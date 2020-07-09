160a58f644493204821e0759fb5408a7b6fa8650
TurBoss <j.l.toledano.l@gmail.com>
2020-07-09 02:35:49 +0200

    rename files in LATC VCP from probe_basic to probe_basic_latc

--- probe_basic_latc/__init__.py
+++ probe_basic_latc/__init__.py
@@ -1,19 +1,23 @@
+import pydevd_pycharm
+pydevd_pycharm.settrace('localhost', port=1337, stdoutToServer=True, stderrToServer=True)
 
-__version__ = '0.0.1'
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
 
 import os
 import qtpyvcp
 
 VCP_DIR = os.path.realpath(os.path.dirname(__file__))
-VCP_CONFIG_FILE = os.path.join(VCP_DIR, 'probe_basic.yml')
+VCP_CONFIG_FILE = os.path.join(VCP_DIR, 'probe_basic_latc.yml')
 
 
 def main(opts=None):
 
     if opts is None:
         from qtpyvcp.utilities.opt_parser import parse_opts
-        opts = parse_opts(vcp_cmd='probebasic',
-                          vcp_name='Probe Basic',
+        opts = parse_opts(vcp_cmd='probe_basic_latc',
+                          vcp_name='Probe Basic Linear ATC',
                           vcp_version=__version__)
 
     qtpyvcp.run_vcp(opts, VCP_CONFIG_FILE)
