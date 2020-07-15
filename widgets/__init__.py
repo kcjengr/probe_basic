baaade606b94657cf7c5cc14f7650d6ca884838a
Josh Handel <jhandel@gmail.com>
2020-07-14 21:42:28 -0500

    move things around for dynamic widget discovery

--- widgets/__init__.py
+++ widgets/__init__.py
@@ -1,16 +1,42 @@
 from qtpyvcp.widgets.qtdesigner import _DesignerPlugin
+import os
 
-from lathe_tool_touch_off.lathe_tool_touch_off import LatheToolTouchOff
-class LatheToolTouchOff_Plugin(_DesignerPlugin):
-    def pluginClass(self):
-        return LatheToolTouchOff
+class PluginFactory():
 
-from atc_widget.atc import DynATC
-class DynATC_Plugin(_DesignerPlugin):
-    def pluginClass(self):
-        return DynATC
+    @staticmethod
+    def import_class(cl):
+        d = cl.rfind(".")
+        classname = cl[d+1:len(cl)]
+        m = __import__(cl[0:d], globals(), locals(), [classname])
+        return getattr(m, classname)
 
-from vcp_jogging.JogControl import JogControl
-class JogControl_Plugin(_DesignerPlugin):
+    @staticmethod
     def pluginClass(self):
-        return JogControl
+        return self.Widget_Type
+
+    @staticmethod
+    def loadPlugin(classpath,pluginname):
+        plugin = type(pluginname, (_DesignerPlugin,), { 
+            # data members  
+            "Widget_Type":PluginFactory.import_class(classpath),
+            
+            # member functions 
+            "pluginClass":PluginFactory.pluginClass
+        })
+        return plugin
+
+    @staticmethod
+    def LoadWidgetsFromPath(widget_folder,scope):
+        folders = [d for d in os.listdir(widget_folder) if os.path.isdir(os.path.join(widget_folder, d))]
+        print "widgets found: " + str(folders)
+        for folder in folders:
+            namespace = folder+"."+folder+"."+folder
+            classname = folder+"_Plugin"
+            scope[classname] = PluginFactory.loadPlugin(namespace,classname)
+
+PluginFactory.LoadWidgetsFromPath(os.path.dirname(os.path.realpath(__file__)),globals())
+
+
+
+
+    
