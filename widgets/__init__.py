bb7cbfcd361d5de8d9c0c3ba060141f08010fbe9
TurBoss <j.l.toledano.l@gmail.com>
2020-09-30 12:59:45 +0200

    widgets path

--- widgets/__init__.py
+++ widgets/__init__.py
@@ -1,11 +1,14 @@
 from qtpyvcp.widgets.qtdesigner import _DesignerPlugin
 
-from lathe_tool_touch_off.lathe_tool_touch_off import LatheToolTouchOff
+from widgets.lathe_tool_touch_off.lathe_tool_touch_off import LatheToolTouchOff
+from widgets.atc_widget.atc import DynATC
+
+
 class LatheToolTouchOff_Plugin(_DesignerPlugin):
     def pluginClass(self):
         return LatheToolTouchOff
 
-from atc_widget.atc import DynATC
+
 class DynATC_Plugin(_DesignerPlugin):
     def pluginClass(self):
         return DynATC
