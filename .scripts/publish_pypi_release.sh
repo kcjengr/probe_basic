f60c434cfbc4459356e2e512391162092ce14567
TurBoss <j.l.toledano.l@gmail.com>
2021-02-19 22:09:23 +0100

    release: pypi auth

--- .scripts/publish_pypi_release.sh
+++ .scripts/publish_pypi_release.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+PYPI_AUTH="$1"
+
 echo 'Installing twine... '
 pip install twine
 
 echo -n 'Using setuptools version: '
-python -c "import setuptools; print setuptools.__version__"
+python -c "import setuptools; print(setuptools.__version__)"
 
 echo 'Uploading files to PyPi...'
 twine upload \
     --username kcjengr \
-    --password $PYPI_PASS \
+    --password $PYPI_AUTH \
     'dist/probe_basic*.tar.gz'
