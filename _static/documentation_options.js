d5d59f38d65e8f2caf02f77b41920a4c3fc0da93
TurBoss <j.l.toledano.l@gmail.com>
2021-04-27 03:24:25 +0200

    deploy gh-pages

---
+++ _static/documentation_options.js
@@ -0,0 +1,10 @@
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: 'v0.3.4+25.ga2ff8ab.dirty',
+    LANGUAGE: 'None',
+    COLLAPSE_INDEX: false,
+    FILE_SUFFIX: '.html',
+    HAS_SOURCE: true,
+    SOURCELINK_SUFFIX: '.txt',
+    NAVIGATION_WITH_KEYS: false,
+};
