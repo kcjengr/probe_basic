4330a633c49b977499aef1df52262f40c12d335d
Lcvette <Lcvette1@gmail.com>
2022-02-03 10:03:12 -0500

    ui changes, added actual spindle display option, created one postgui hal

--- probe_basic/probe_basic.py
+++ probe_basic/probe_basic.py
@@ -35,6 +35,10 @@ class ProbeBasic(VCPMainWindow):
         self.sidebar_widget.setCurrentIndex(button.property('page'))
 
     @Slot(QAbstractButton)
+    def on_spindlerpmsourcebtnGroup_buttonClicked(self, button):
+        self.spindle_rpm_source_widget.setCurrentIndex(button.property('page'))
+
+    @Slot(QAbstractButton)
     def on_gcodemdibtnGroup_buttonClicked(self, button):
         self.gcode_mdi.setCurrentIndex(button.property('page'))
 
