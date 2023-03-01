21cf118f184d23be9c04a8c2337f64f757946982
lcvette <lcvette1@gmail.com>
2023-03-01 00:19:23 -0500

    working .deb package build with good working install

--- setup.py
+++ setup.py
@@ -5,5 +5,24 @@ setup(
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
     zip_safe=True,
-    packages=['widgets', 'probe_basic', 'probe_basic.fonts', 'probe_basic.images', 'probe_basic.virtual_keyboards', 'example_gcode', 'probe_basic_latc', 'probe_basic_lathe', 'probe_basic_lathe.images','probe_basic_vertical', 'widgets.atc_widget', 'widgets.atc_widget.images', 'widgets.conversational', 'widgets.conversational.images', 'widgets.lathe_tool_touch_off', 'widgets.lathe_tool_touch_off.images']
+    packages=[
+        'debian',
+        'example_gcode',
+        'probe_basic',
+        'probe_basic.fonts',
+        'probe_basic.images',
+        'probe_basic.virtual_keyboards',
+        'probe_basic_latc',
+        'probe_basic_latc.images',
+        'probe_basic_lathe',
+        'probe_basic_lathe.images',
+        'probe_basic_vertical',
+        'probe_basic_vertical.images',
+        'widgets',
+        'widgets.atc_widget',
+        'widgets.atc_widget.images',
+        'widgets.conversational',
+        'widgets.conversational.images',
+        'widgets.lathe_tool_touch_off',
+        'widgets.lathe_tool_touch_off.images']
 )
