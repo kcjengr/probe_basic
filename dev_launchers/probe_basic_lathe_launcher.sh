65afd4f749b8c7164598741c2bf123078974317b
dpslwk <dps.lwk@gmail.com>
2023-11-12 15:45:33 +0000

    Subroutine merge: 1: move configs/ under linuxcnc/

--- dev_launchers/probe_basic_lathe_launcher.sh
+++ dev_launchers/probe_basic_lathe_launcher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source ~/dev/venv/bin/activate
-cd ~/dev/probe_basic/configs/probe_basic_lathe
+cd ~/dev/probe_basic/linuxcnc/configs/probe_basic_lathe
 linuxcnc probe_basic_lathe.ini
--- dev_launchers/probe_basic_mill_launcher.sh
+++ dev_launchers/probe_basic_mill_launcher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 source ~/dev/venv/bin/activate
-cd ~/dev/probe_basic/configs/probe_basic
+cd ~/dev/probe_basic/linuxcnc/configs/probe_basic
 linuxcnc probe_basic.ini
