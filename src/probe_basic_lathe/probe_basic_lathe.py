25fde761bf8c45bffeeed6452c27f4c00f14c4de
Chris Polanski <small.shop.concepts@gmail.com>
2025-06-05 01:33:39 -0400

    update mdi entry widget to extended selection type

--- src/probe_basic_lathe/probe_basic_lathe.py
+++ src/probe_basic_lathe/probe_basic_lathe.py
@@ -33,10 +33,6 @@ class ProbeBasicLathe(VCPMainWindow):
     def __init__(self, *args, **kwargs):
         super(ProbeBasicLathe, self).__init__(*args, **kwargs)
         self.run_from_line_Num.setValidator(QRegExpValidator(QRegExp("[0-9]*")))
-        self.feed_unit_per_minute = 0.0
-        self.feed_per_rev = 0.0
-        self.css_sword = 0.0
-        self.rpm_mode = 0.0
         self.btnMdiBksp.clicked.connect(self.mdiBackSpace_clicked)
         self.btnMdiSpace.clicked.connect(self.mdiSpace_clicked)
         
@@ -207,46 +203,6 @@ class ProbeBasicLathe(VCPMainWindow):
         if sidebar_loaded == False:
             self.user_sb_tab.hide()
             self.dro_tab.setStyleSheet(self.user_sb_tab.styleSheet())
-
-    def on_feed_unit_per_minute_entry_textChanged(self, value):
-        if value:
-            self.feed_unit_per_minute = float(value)
-        else:
-            self.feed_unit_per_minute = 0.0
-
-    def on_feed_unit_per_minute_entry_returnPressed(self):
-        cmd = "G94 F{}".format(self.feed_unit_per_minute)
-        issue_mdi(cmd)
-
-    def on_feed_per_rev_entry_textChanged(self, value):
-        if value:
-            self.feed_per_rev = float(value)
-        else:
-            self.feed_per_rev = 0.0  # Consistent with other methods
-
-    def on_feed_per_rev_entry_returnPressed(self):
-        cmd = "G95 F{}".format(self.feed_per_rev)
-        issue_mdi(cmd)
-
-    def on_css_sword_entry_textChanged(self, value):
-        if value:
-            self.css_sword = float(value)
-        else:
-            self.css_sword = 0.0
-
-    def on_css_sword_entry_returnPressed(self):
-        cmd = "G96 S{}".format(self.css_sword)
-        issue_mdi(cmd)
-
-    def on_rpm_mode_entry_textChanged(self, value):
-        if value:
-            self.rpm_mode = float(value)
-        else:
-            self.rpm_mode = 0
-
-    def on_rpm_mode_entry_returnPressed(self):
-        cmd = "G97 S{}".format(self.rpm_mode)
-        issue_mdi(cmd)
 
     def on_use_tcp_clicked(self):
         if self.use_tcp.isChecked():
