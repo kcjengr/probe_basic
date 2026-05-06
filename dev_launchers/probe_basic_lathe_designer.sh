8a0160e14361c6126405577fc3d485ce3df38161
Chris Polanski <small.shop.concepts@gmail.com>
2026-05-05 23:13:58 -0400

    update qss to handle comboboxes with new dynamic settings and lathe/conv

--- dev_launchers/probe_basic_lathe_designer.sh
+++ dev_launchers/probe_basic_lathe_designer.sh
@@ -3,4 +3,7 @@
 source ~/dev/venv/bin/activate
 cd ~/dev/
 
+# Include primary lathe_conv config so settings appear in Designer tools
+export VCP_CONFIG_FILES="${VCP_CONFIG_FILES:+${VCP_CONFIG_FILES}:}${HOME}/dev/pb_lathe_conv/src/lathe_conversational/lathe_conv.yml"
+
 editvcp probe_basic_lathe
