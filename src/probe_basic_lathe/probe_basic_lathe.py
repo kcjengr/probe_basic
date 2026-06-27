57530efe921f79148742cdba71a4a62d5079be8c
Chris Polanski <small.shop.concepts@gmail.com>
2026-06-27 14:56:27 -0400

    update conv connections

--- src/probe_basic_lathe/probe_basic_lathe.py
+++ src/probe_basic_lathe/probe_basic_lathe.py
@@ -780,10 +780,10 @@ class ProbeBasicLathe(VCPMainWindow):
 
         addon_module = importlib.import_module(module_name)
         widget_class = getattr(addon_module, "LatheConv")
-        widget = widget_class(parent=self)
-        if not getattr(widget, '_license_valid', False):
-            LOG.warning("lathe_conversational license invalid; conversational tab will not be shown")
-            widget.deleteLater()
+        try:
+            widget = widget_class(parent=self)
+        except RuntimeError as exc:
+            LOG.warning(f"lathe_conversational license invalid: {exc}")
             return
         self.tabWidget.addTab(widget, "CONVERSATIONAL")
         LOG.info("Loaded lathe_conversational addon tab")
